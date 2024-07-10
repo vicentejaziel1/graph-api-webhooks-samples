2768a59a88dcf0a1c33b91369a25a91ef5875472
vicentejaziel1 <39107638+vicentejaziel1@users.noreply.github.com>
2024-07-10 13:36:41 -0600

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -14,7 +14,7 @@ var xhub = require('express-x-hub');
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(xhub({ algorithm: 'sha256', secret: process.env.APP_SECRET }));
 console.log('SECRET', process.env.APP_SECRET);
 app.use(bodyParser.json());
 
